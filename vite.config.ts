6a29ae80228e3b5a58a3b7678cdc7e2435d9ed4d
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-03-20 23:29:01 +0100

    Reverted build changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/bricks/',
   plugins: [tailwindcss()],
-  build: {
-    outDir: 'docs',
-  }
 })
